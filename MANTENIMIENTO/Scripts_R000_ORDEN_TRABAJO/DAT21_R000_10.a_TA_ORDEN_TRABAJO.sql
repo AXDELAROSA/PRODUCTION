5eb488afe372949cd0af0441bfeeb13b4cc590b9
AXDELAROSA <48331083+AXDELAROSA@users.noreply.github.com>
2021-11-02 07:35:43 -0600

    Update DAT21_R000_10.a_TA_ORDEN_TRABAJO.sql

--- MANTENIMIENTO/Scripts_R000_ORDEN_TRABAJO/DAT21_R000_10.a_TA_ORDEN_TRABAJO.sql
+++ MANTENIMIENTO/Scripts_R000_ORDEN_TRABAJO/DAT21_R000_10.a_TA_ORDEN_TRABAJO.sql
@@ -14,12 +14,12 @@ GO
 -- //////////////////////////////////////////////////////////////
 -- // DROPs
 -- //////////////////////////////////////////////////////////////
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ORDEN_TRABAJO_DETALLE]') AND type in (N'U'))
-	DROP TABLE [dbo].[ORDEN_TRABAJO_DETALLE]
-GO
-
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ORDEN_TRABAJO]') AND type in (N'U'))
 	DROP TABLE [dbo].[ORDEN_TRABAJO]
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ORDEN_TRABAJO_DETALLE]') AND type in (N'U'))
+	DROP TABLE [dbo].[ORDEN_TRABAJO_DETALLE]
 GO
 
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[STATUS_ORDEN_TRABAJO]') AND type in (N'U'))
@@ -93,13 +93,58 @@ ELSE
 
 GO
 
-EXECUTE [dbo].[PG_CI_STATUS_ORDEN_TRABAJO] 0,139,00, 'CANCELADA',					'', 'CANCL', 00,0		-- ACTUALIZA QUIEN LA GENERA
+EXECUTE [dbo].[PG_CI_STATUS_ORDEN_TRABAJO] 0,139,00, 'CANCELADA',					'', 'CANCL',	00,0		-- ACTUALIZA QUIEN LA GENERA
 -- =================================================================================
-EXECUTE [dbo].[PG_CI_STATUS_ORDEN_TRABAJO] 0,139,01, 'CREADA',						'', 'CREAD', 10,1		-- ESTATUS INICIAL
-EXECUTE [dbo].[PG_CI_STATUS_ORDEN_TRABAJO] 0,139,01, 'ENVIADA',						'', 'ENVIADA', 10,1		-- ENVIADA
-EXECUTE [dbo].[PG_CI_STATUS_ORDEN_TRABAJO] 0,139,02, 'CERRADA COMPLETA',			'', 'COMPL', 20,0		-- CERRADA COMPLETA
-EXECUTE [dbo].[PG_CI_STATUS_ORDEN_TRABAJO] 0,139,03, 'CERRADA PARCIAL',				'', 'PARCL', 20,0		-- CERRADA PARCIAL
+EXECUTE [dbo].[PG_CI_STATUS_ORDEN_TRABAJO] 0,139,10, 'CREADA',						'', 'CREAD',	10,1		-- ESTATUS INICIAL
+EXECUTE [dbo].[PG_CI_STATUS_ORDEN_TRABAJO] 0,139,20, 'ENVIADA',						'', 'ENVDA',	20,1		-- ENVIADA: DESDE TPO_CUSTOMER O POR REPARACIÓN DE DADOS.
+EXECUTE [dbo].[PG_CI_STATUS_ORDEN_TRABAJO] 0,139,30, 'ASIGNADA',					'', 'ASIGN',	30,1		-- ASIGNADA A UN TÉCNICO.
+EXECUTE [dbo].[PG_CI_STATUS_ORDEN_TRABAJO] 0,139,40, 'TERMINADA',					'', 'TRMIN',	40,1		-- TERMINADA
+EXECUTE [dbo].[PG_CI_STATUS_ORDEN_TRABAJO] 0,139,50, 'RECHAZADA',					'', 'RCHAZ',	50,1		-- RECHAZADA: REQUIERE CAMBIOS O NO SE HIZO CORRECTAMENTE.
+EXECUTE [dbo].[PG_CI_STATUS_ORDEN_TRABAJO] 0,139,60, 'CERRADA',						'', 'COMPL',	60,0		-- CERRADA COMPLETA, EL USUARIO ACEPTÓ EL SERVICIO.
+EXECUTE [dbo].[PG_CI_STATUS_ORDEN_TRABAJO] 0,139,70, 'CERRADA PARCIAL',				'', 'PARCL',	70,0		-- CERRADA PARCIAL
 -- =================================================================================
+GO
+
+
+-- ////////////////////////////////////////////////////////////////
+-- //					TIPO_ORDEN_TRABAJO
+-- ////////////////////////////////////////////////////////////////
+CREATE TABLE [dbo].[TIPO_ORDEN_TRABAJO] (
+	[K_TIPO_ORDEN_TRABAJO]				[INT] IDENTITY (1,1)	NOT NULL,
+	-- ============================
+	[D_TIPO_ORDEN_TRABAJO]				[VARCHAR] (500) NOT NULL
+	-- ============================
+) ON [PRIMARY]
+GO
+
+
+-- ////////////////////////////////////////////////////////////////
+-- //					FALLA_ORDEN_TRABAJO
+-- ////////////////////////////////////////////////////////////////
+CREATE TABLE [dbo].[FALLA_TRABAJO_DETALLE] (
+	[K_FALLA_ORDEN_TRABAJO]				[INT] IDENTITY (1,1)	NOT NULL,
+	-- ============================
+	[D_FALLA_ORDEN_TRABAJO]				[VARCHAR] (500) NOT NULL
+	-- ============================
+) ON [PRIMARY]
+GO
+
+
+-- ////////////////////////////////////////////////////////////////
+-- //					ORDEN_TRABAJO_DETALLE
+-- ////////////////////////////////////////////////////////////////
+CREATE TABLE [dbo].[ORDEN_TRABAJO_DETALLE] (
+	[K_ORDEN_TRABAJO_DETALLE]			[INT] IDENTITY (1,1)	NOT NULL,
+	[K_ORDEN_TRABAJO]					[INT] NOT NULL,
+	-- ============================
+	[N_TECNICO_REALIZA]					[INT] NOT NULL,
+	[D_TECNICO_REALIZA]					[VARCHAR] (500) NOT NULL,	
+	-- ============================
+	[DURACION_MINUTOS]					[INT] NOT NULL,
+	[ACCION_REALIZADA]					[NVARCHAR](MAX) NOT NULL DEFAULT '',
+	[C_ORDEN_TRABAJO]					[NVARCHAR](MAX) NOT NULL DEFAULT ''	
+	-- ============================
+) ON [PRIMARY]
 GO
 
 
@@ -109,20 +154,32 @@ GO
 CREATE TABLE [dbo].[ORDEN_TRABAJO] (
 	[K_ORDEN_TRABAJO]						[INT] IDENTITY (1,1)	NOT NULL,
 	-- ============================
-	[AREA_ORDEN_TRABAJO]					[VARCHAR](500) NOT NULL,
-	[F_DATE_ORDEN_TRABAJO]					[DATE] NOT NULL,
-	[D_USUARIO_SOLICITA]					[VARCHAR](500) NOT NULL,
+	[K_USUARIO_SOLICITA]					[INT] NOT NULL,
+	[K_USUARIO_RECIBE]						[INT] NOT NULL,
 	-- ============================
-	[D_TECNICO_REALIZA]						[VARCHAR](500) NOT NULL,
-	[DURACION_ORDEN_TRABAJO]				[VARCHAR](500) NOT NULL,
-	[D_USUARIO_RECIBE]						[VARCHAR](500) NOT NULL,
+	[K_ESTACION]							[INT] NOT NULL,
+	[K_SUB_ESTACION]						[INT] NOT NULL,
 	-- ============================
 	[K_STATUS_ORDEN_TRABAJO]				[INT] NOT NULL	DEFAULT 1,
 	-- ============================
-	[D_SISTEMA_ADICIONAL]					[VARCHAR](500) NOT NULL,
-	[K_SISTEMA_ADICIONAL]					[INT] NOT NULL	DEFAULT 0,
+	[D_ORDEN_TRABAJO]						[NVARCHAR](MAX) NOT NULL DEFAULT '',
+	[F_ORDEN_TRABAJO]						[DATE] NOT NULL,
 	-- ============================
+	[DURACION_TOTAL_MINUTOS]				[INT] NOT NULL,
+	-- ============================	
+	[K_TIPO_ORDEN_TRABAJO]					[INT] NOT NULL	DEFAULT 1,
+	[K_FALLA_ORDEN_TRABAJO]					[INT] NOT NULL	DEFAULT 1,
+	-- ============================	
 	[RUTA_ORDEN_TRABAJO]					[NVARCHAR](MAX) NULL
+	-- ============================
+	--[AREA_ORDEN_TRABAJO]					[VARCHAR](500) NOT NULL,
+	--[D_USUARIO_SOLICITA]					[VARCHAR](500) NOT NULL,
+	---- ============================
+	--[D_TECNICO_REALIZA]					[VARCHAR](500) NOT NULL,
+	--[D_USUARIO_RECIBE]					[VARCHAR](500) NOT NULL,
+	---- ============================
+	--[D_SISTEMA_ADICIONAL]					[VARCHAR](500) NOT NULL,
+	--[K_SISTEMA_ADICIONAL]					[INT] NOT NULL	DEFAULT 0,
 ) ON [PRIMARY]
 GO
 -- //////////////////////////////////////////////////////
@@ -145,27 +202,10 @@ ALTER TABLE [dbo].[ORDEN_TRABAJO]
 			[K_USUARIO_BAJA]			[INT] NULL,
 			[F_BAJA]					[DATETIME] NULL;
 GO
-
-
--- ////////////////////////////////////////////////////////////////
--- //					ORDEN_TRABAJO_DETALLE
--- ////////////////////////////////////////////////////////////////
-CREATE TABLE [dbo].[ORDEN_TRABAJO_DETALLE] (
-	[K_ORDEN_TRABAJO_DETALLE]				[INT] IDENTITY (1,1)	NOT NULL,
-	[K_ORDEN_TRABAJO]						[INT] NOT NULL,
-	-- ============================
-	[DESCRIPCION_ORDEN_TRABAJO]				[NVARCHAR](MAX) NOT NULL DEFAULT '',
-	[ACCIONES_ORDEN_TRABAJO]				[NVARCHAR](MAX) NOT NULL DEFAULT '',
-	[C_ORDEN_TRABAJO]						[NVARCHAR](MAX) NOT NULL DEFAULT ''
-	-- ============================
-) ON [PRIMARY]
-GO
-
-
 ---- //////////////////////////////////////////////////////////////
 ---- //////////////////////////////////////////////////////////////
 ---- //////////////////////////////////////////////////////////////
 
-UPDATE	TPO_CUSTOMER_DET_SET_COTIZADO
-SET		K_STATUS_SET_COTIZADO	= 1
-WHERE	K_STATUS_SET_COTIZADO	= 2
+--UPDATE	TPO_CUSTOMER_DET_SET_COTIZADO
+--SET		K_STATUS_SET_COTIZADO	= 1
+--WHERE	K_STATUS_SET_COTIZADO	= 2
